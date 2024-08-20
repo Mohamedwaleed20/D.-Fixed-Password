bfe1ce382f03ec93bdf4b63c968834308803b5ca
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-21 02:53:58 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+   while(true)
+   {
+       int n ;
+       cin >> n ;
+       int code = 1999 ;
+       if(n!=code)
+       {
+           cout << "Wrong" << endl ;
+       }
+       else
+       {
+           cout << "Correct" << endl ;
+           return 0;
+       }
+   }
+
+
+    return 0;
+}
